0e667f325c83cbb2f5b7ee3843b4b877edae846b
Blake Nuttall <blakedyblake@gmail.com>
2021-08-13 10:30:00 -0600

    All finsihedgit add process.py

--- process.py
+++ process.py
@@ -10,3 +10,18 @@ def sales_reports(log_file): #creates function that uses file
 
 
 sales_reports(log_file) #calls the function
+log_file.close
+
+log_file = open("um-server-01.txt") 
+
+def getOver(log_file):
+    for line in log_file:
+        line = line.split(" ")
+        melonNum = int(line[2])
+        if(melonNum > 10):
+            print(line)
+
+
+getOver(log_file)
+
+log_file.close
