08e8ea5524f64c2744ab6db672e7096f4802ca67
Blake Nuttall <blakedyblake@gmail.com>
2021-08-13 10:21:17 -0600

    Finished extra credit for sql

--- sql-skills.sql
+++ sql-skills.sql
@@ -32,3 +32,38 @@ SELECT customer.first_name "Customer Fistname", customer.last_name"Customer last
 JOIN employee ON customer.support_rep_id = employee.employee_id;
 
 SELECT album.title, artist.name FROM album JOIN artist ON artist.artist_id =album.artist_id;
+
+
+--Extra credit
+SELECT * FROM artist  order by name desc LIMIT 10;
+SELECT * FROM artist  WHERE name like 'Black%';
+SELECT * FROM artist  WHERE name like '%Black%';
+
+SELECT MAX(birth_date) FROM employee;
+SELECT MIN(birth_date) FROM employee;
+
+SELECT COUNT(*) FROM invoice WHERE billing_state IN('CA', 'TX', 'AZ');
+SELECT AVG(total) FROM invoice;
+
+
+--JOIN
+SELECT playlist_track.track_id FROM playlist_track 
+JOIN playlist ON playlist.playlist_id = playlist_track.playlist_id
+WHERE playlist.name = 'Music';
+
+SELECT track.name FROM TRACK
+JOIN playlist_track ON playlist_track.track_id = track.track_id
+JOIN playlist ON playlist_track.playlist_id = playlist.playlist_id
+WHERE playlist.playlist_id = 5;
+
+SELECT track.name "Song", playlist.name "Playlist" FROM track
+JOIN playlist_track ON playlist_track.track_id = track.track_id
+JOIN playlist ON playlist_track.playlist_id = playlist.playlist_id;
+
+SELECT track.name "Song", album.title "Album" FROM track
+JOIN album ON track.album_id = album.album_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name IN('Alternative', 'Punk');
+
+
+
